a77adf0f1382ab0153ce694cfa5eb5db530abe77
Nitin0202 <nitinsachdeva0202@gmail.com>
2024-09-07 01:13:26 +0530

    updaets

--- src/components/UI/blog.js
+++ src/components/UI/blog.js
@@ -5,16 +5,13 @@ const Blog = async () => {
     const res = await axios.get(`${process.env.BASE_URL}/api/blog`);
     const { allPost: posts } = res.data;
     return (
-        <div className=' w-[95%] lg:w-[75%] m-auto rounded  bg-transparent '>
-            <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2  xl:grid-cols-3 gap-6 p-2 bg-transparent'>
-                {
-                    posts.map((item, index) => {
-                        return <BlogCard index={ index } data={ item } />
-                    })
-                }
+        <div className='w-[95%] lg:w-[75%] m-auto rounded bg-transparent'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 p-2 bg-transparent'>
+                { posts.map((item) => (
+                    <BlogCard key={ item.id } data={ item } /> // Add a unique key prop here
+                )) }
             </div>
         </div>
     )
 }
-
 export default Blog
