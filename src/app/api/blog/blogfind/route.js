caa416648c9789b1a1dbea33315e28de33be91db
Nitin0202 <nitinsachdeva0202@gmail.com>
2024-09-07 11:51:32 +0530

    api isseu fixed

--- src/app/api/blog/blogfind/route.js
+++ src/app/api/blog/blogfind/route.js
@@ -4,12 +4,16 @@ import { NextResponse } from "next/server";
 export async function POST(req, res) {
     try {
         const { id } = await req.json();
+        console.log(id);
         if (!id) {
             return NextResponse.json({ message: 'id is required.' }, { status: 400 });
         }
         const data = await Blog.findById(id);
+        console.log(data, 'data');
+
         return NextResponse.json({ message: 'data post succefull', data });
     } catch (error) {
+        console.log(error);
         return NextResponse.json({ message: 'data post faild to find', error }, { status: 400 });
     }
 }
