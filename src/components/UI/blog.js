3d4df2764587556211b0267c62f6679f2344b702
Nitin0202 <nitinsachdeva0202@gmail.com>
2024-09-07 10:42:17 +0530

    add new updates comment code

--- src/components/UI/blog.js
+++ src/components/UI/blog.js
@@ -2,16 +2,17 @@ import BlogCard from './blogCard'
 import axios from 'axios'
 
 const Blog = async () => {
-    const res = await axios("/api/blog");
-    const { allPost: posts } = res.data;
+    // const res = await axios("/api/blog");
+    // const { allPost: posts } = res.data;
+
     return (
         <div className='w-[95%] lg:w-[75%] m-auto rounded bg-transparent'>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 p-2 bg-transparent'>
-                { posts.map((item, index) => {
+                {/* { posts.map((item, index) => {
                     return (
                         <BlogCard key={ item.id } data={ item } />
                     )
-                }) }
+                }) } */}
             </div>
         </div>
     )
